4c2034d33cc11920607fdec5caa81adc6d515145
DianaRobayo <dmrcupn92@gmail.com>
2024-05-27 22:19:45 -0500

    Edit step of a CRUD is created

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Person } from './Person';
 
-export const Persons = () => {
+export const Persons = ({ persons, setPersons }) => {
 
-  const [ persons, setPersons ] = useState([
-    {
-      id: 1,
-      name: "Oscar Eduardo",
-      role: "Frontend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar6.png",
-    },
-    {
-      id: 2,
-      name: "Carolina",
-      role: "Backend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    },
-    {
-      id: 3,
-      name: "Catalina",
-      role: "UI/UX Designer",
-      img: "https://bootdey.com/img/Content/avatar/avatar8.png",
-    },
-  ]);
+  const [editingId, setEditingId] = useState(null);
+  const [editedPerson, setEditedPerson] = useState({
+    name: "Oscar",
+    role: "Frontend Developer",
+    img: "https://bootdey.com/img/Content/avatar/avatar6.png"
+  });
+  const [isEditing, setIsEditing] = useState(false);
+
+  // handle son eventos
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedPerson(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const handleEdit = (id) => {
+    // Establece el id de la persona que queremos editar
+    setEditingId(id);
+    // Activamos el estado de edición
+    setIsEditing(true);
+    // Buscamos la persona a editar por medio del id y la asigna a personToEdit
+    const personToEdit = persons.find(obj => obj.id === id);
+    // Establecemos los datos de la persona a editar
+    setEditedPerson({ ...personToEdit });
+  }
+
+  const handleSave = (e) => {
+    setPersons(persons.map(person => person.id === editingId ? editedPerson : person));
+    setEditingId(null);
+    setEditedPerson({ name: '', role: '', img: ''});
+    setIsEditing(false);
+  }
 
   return (
     <div>
@@ -34,15 +47,39 @@ export const Persons = () => {
             return (
               <div>
                 <Person
-                  key={person.id}
+                  id={person.id}
                   name={person.name}
                   role={person.role}
                   img={person.img}
+                  funcion={() => handleEdit(person.id)}
                 />
               </div>
             )
           })}
         </div>
+      </div>
+      <div className='mt-4'>
+        <h2>Modificar datos</h2>
+        <div className='container text-center'>
+          <div className='row justify-content-md-center'>
+            <div className='col-8'>
+              <input type="text" className='form-control' name='name'
+                placeholder='Nombre' value={editedPerson.name}
+                onChange={handleChange} />
+
+              <input type="text" className='form-control' name='role'
+                placeholder='Rol' value={editedPerson.role} onChange={handleChange} />
+
+              <input type="text" className='form-control' name='img'
+                placeholder='Url de la imagen' value={editedPerson.img} onChange={handleChange} />
+            </div>
+          </div>
+          <div className='mt-2'>
+            <button className='btn btn-primary' onClick={handleSave}>
+              Guardar
+            </button>
+          </div>
+        </div>  
       </div>
     </div>
   )
